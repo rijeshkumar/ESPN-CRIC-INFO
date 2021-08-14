25cd1ce683de994a4b5c8834c2f501ff9b7ce0da
yashoswalyo <yashoswal18@gmail.com>
2021-08-14 17:58:46 +0530

    added bot vars

---
+++ bot.py
@@ -0,0 +1,16 @@
+import pyrogram
+from pyrogram import Client
+
+
+from config import Config, Translation
+
+app = Client (
+    session_name='ESPN',
+    api_id=Config.APP_ID,
+    api_hash=Config.API_HASH,
+    bot_token=Config.TG_BOT_TOKEN,
+    plugins=dict(root="plugins")
+    )
+
+if __name__ == "__main__":
+	app.run()
